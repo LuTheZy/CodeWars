1eea05fa43ca58520eca44d484bd2bba6e1659d8
Ciaran Alexander <ciarana09@gmail.com>
2019-08-02 15:07:22 +0200

    --Add Century Calculator and Jagged Array handler--

---
+++ CodeWars.CenturyCalculator/CenturyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.CenturyCalculator
+{
+    public class CenturyConverter
+    {
+        public static int CalculateCentury(int year)
+        {
+            return (year / 100) + ((year % 100 == 0) ? 0 : 1);
+        }
+    }
+}
---
+++ CodeWars.Tests/JaggedArrayTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars.JaggedArrays;
+using NUnit.Framework;
+
+namespace CodeWars.Tests
+{
+    public class JaggedArrayTests
+    {
+        [Test]
+        public void JaggedArray_Should_Return_Correct_Classifications()
+        {
+            Assert.AreEqual(new[] { "Open", "Senior", "Open", "Senior" }, MemberClassifier.OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+            Assert.AreEqual(new[] { "Open", "Open", "Open", "Open" }, MemberClassifier.OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }));
+            Assert.AreEqual(new[] { "Senior", "Open", "Open", "Open" }, MemberClassifier.OpenOrSenior(new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } }));
+        }
+    }
+}
